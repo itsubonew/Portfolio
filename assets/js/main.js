d37aaa5324aae908072fc8e629e57705be98732d
itsubonew <itsubonew@gmail.com>
2023-05-08 11:25:18 -0700

    main.js edited

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const showManu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showManu('nav-toggle','nav-menu')
